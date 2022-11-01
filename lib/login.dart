edbb74ed5c17e92a29a2d5d7d65da318f95b14aa
muyaho-universe <kimdaesk@gmail.com>
2022-11-01 22:03:52 +0900

    Convert mdc to 어쩔맛집

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,55 +13,121 @@ class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  // Future<UserCredential> signInWithGoogle() async {
+  //   // Trigger the authentication flow
+  //   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  //
+  //   // Obtain the auth details from the request
+  //   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+  //
+  //   // Create a new credential
+  //   final credential = GoogleAuthProvider.credential(
+  //     accessToken: googleAuth?.accessToken,
+  //     idToken: googleAuth?.idToken,
+  //   );
+  //
+  //   // Once signed in, return the UserCredential
+  //   return await FirebaseAuth.instance.signInWithCredential(credential);
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            const SizedBox(height: 80.0),
+            SizedBox(height: 100.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  '어 쩔 맛 집',
+                  style: TextStyle(
+                    fontFamily: 'Baemin',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
               ],
             ),
-            const SizedBox(height: 120.0),
+            SizedBox(height: 52.0),
             TextField(
               controller: _usernameController,
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Username',
+              decoration: InputDecoration(
+                labelText: '이메일',
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Color(0xffDE5E56),
+                    )),
               ),
             ),
-            const SizedBox(height: 12.0),
+            SizedBox(height: 25.0),
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Password',
+              decoration: InputDecoration(
+                labelText: '비밀번호',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Color(0xffDE5E56),
+                  ),
+                ),
               ),
               obscureText: true,
             ),
-            OverflowBar(
-              alignment: MainAxisAlignment.end,
-              children: <Widget>[
+            SizedBox(
+              height: 9,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
                 TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    _usernameController.clear();
-                    _passwordController.clear();
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text('NEXT'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+                  onPressed: () {},
+                  child: Text(
+                    '비밀번호 찾기',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: 66,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('로그인'),
+              style: ElevatedButton.styleFrom(
+                //padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                backgroundColor: Color(0xffDE5E56),
+              ),
+            ),
+            SizedBox(
+              height: 17,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                '회원가입',
+                style: TextStyle(
+                  color: Color(0xffDE5E56),
+                ),
+              ),
+            ),
+            SizedBox(height: 62),
+            Text(
+              '간단하게 SNS 회원가입',
+              style: TextStyle(
+                fontSize: 14,
+              ),
             ),
           ],
         ),
